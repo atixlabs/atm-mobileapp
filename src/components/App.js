c18cea5e43b9ac3bb10f36e117288fe591acbb4f
mk <turco807@gmail.com>
2017-07-15 11:26:58 -0300

    Generate wallet and sign a dummy ETH Tx

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ import bip39 from 'bip39';
 import buffer from 'buffer';
 import util from 'ethereumjs-util';
 // import lightwallet from 'eth-lightwallet'; // doesn't work =(
-import ethereumWallet from 'ethereumjs-wallet';
-import ethereumTx from 'ethereumjs-tx';
+import hdkey from 'ethereumjs-wallet/hdkey';
+import EthereumTx from 'ethereumjs-tx';
 
 
 const styles = StyleSheet.create({
@@ -34,10 +34,62 @@ const styles = StyleSheet.create({
   },
 });
 
-const randomBytes = crypto.randomBytes(16);
-const mnemonic = bip39.entropyToMnemonic(randomBytes.toString('hex'))
+/*
+  // Naranja usefull code ?)
+  self.pushContractTransaction = function(_keys, transaction) {
+    $log.info("[backend - pushContractTransaction] Starting", "transaction[" + angular.toJson(transaction) + ']');
+    return self.signHash(transaction.info.to_sign, _keys.privkey)
+    .then(function (signature) {
+      $log.info("[backend - pushContractTransaction] Info", "Calling apiCall(/push_tx)");
+      transaction.signature = signature.signature;
+      return self.apiCall(_keys, ENVIRONMENT.apiurl('/push_tx'), angular.toJson(transaction));
+    });
+  }
+  self.callContractFunction = function(_keys, token_id, fnc, payload) {
+    $log.info("[backend - callContractFunction] Starting", "function[" + fnc + "]" , "payload[" + angular.toJson(payload) + ']');
+    return self.apiCall(_keys, ENVIRONMENT.apiurl('/token/' + token_id + '/' + fnc), payload)
+    .then(function(transaction) {
+      return self.pushContractTransaction(_keys, transaction);
+    });
+  }
+*/
+
+
+const generateWallet = () => {
+  console.log(hdkey);
+  const randomBytes = crypto.randomBytes(16);
+  const mnemonic = bip39.entropyToMnemonic(randomBytes.toString('hex'));
+  const seed = bip39.mnemonicToSeed(mnemonic);
+  const ethKey = hdkey.fromMasterSeed(seed);
+  const wallet = ethKey.getWallet();
+  return {
+    mnemonic: mnemonic,
+    privateKey: wallet.getPrivateKey(),
+    publicKey: wallet.getPublicKeyString().substr(2)
+  }
+};
 
 App = () => {
+  const wallet = generateWallet();
+  console.log("mnemonic", wallet.mnemonic); 
+
+  // dummy tx test:
+  const txParams = {
+    nonce: '0x00',
+    gasPrice: '0x09184e72a000', 
+    gasLimit: '0x2710',
+    to: '0x0000000000000000000000000000000000000000', 
+    value: '0x00', 
+    data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
+    // EIP 155 chainId - mainnet: 1, ropsten: 3
+    chainId: 3
+  };
+
+  const tx = new EthereumTx(txParams);
+  tx.sign(wallet.privateKey);
+  const serializedTx = tx.serialize();
+  console.log("serializedTx", serializedTx);
+
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>
