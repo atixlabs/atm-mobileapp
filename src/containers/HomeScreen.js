73b99ee96c3691e4ea1f57b5f6c5208511073337
Juani <jicostanzo@atixlabs.com>
2017-07-22 12:59:14 -0300

    Add OneSignal package.

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -3,12 +3,25 @@ import {
   Text,
   View,
 } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
 export default class HomeScreen extends React.Component {
-  
   static navigationOptions = {
     title: 'Human ATM',
   };
+
+  componentWillMount() {
+    console.log('daskldjkasldjlaskjdklasjdkal');
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
 
   render() {
     const { navigate } = this.props.navigation;
